de4989767aeda628084310aa50c9e61855f5142c
Alexey Ageev <pochtovii.1996@mail.ru>
2023-02-07 21:52:22 +0300

    add: send data from form to telegram

--- handler.php
+++ handler.php
@@ -1,9 +1,18 @@
-<? php
+<?php
 $user_name = htmlspecialchars($_POST["username"]);
 $user_phone = htmlspecialchars($_POST["userphone"]);
 
 $token = "6143159372:AAEyQZH_6MCc3o29fd73sZ4U64WkXuV9M2c";
 $chat_id = "-840169689";
+
+$formData = array(
+  "Клиент: " => $user_name,
+  "Телефон: " => $user_phone
+);
+
+foreach($formData as $key => $value) {
+  $text .= $key ."<b>" . urlencode($value) . "</b>" . "%0A";
+}
 
 $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&text={$text}&parse_mode=html", "r");
 
@@ -12,3 +21,4 @@ if($sendToTelegram) {
 } else {
   echo "Error";
 }
+
