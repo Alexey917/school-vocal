91664150350a6a7c6bfb3373f60afee1a68f7308
Alexey Ageev <pochtovii.1996@mail.ru>
2023-03-25 01:02:02 +0300

    add: Smooth scroll transition when clicking a link to a block

--- js/main.js
+++ js/main.js
@@ -561,4 +561,35 @@ document.addEventListener("input", (e) => {
   }
 });
 
+const linksHead = document.querySelectorAll(".navbar-list-item-link");
+const linksHeadMobile = document.querySelector(".mobile-menu-link");
+/* спрет оператор: создаем новый массив и добавляем в него массив ссылок и объект с классом main__scroll */
+const newArray = [...linksHead, linksHeadMobile];
+
+//console.log(seamless);
+
+newArray.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    /* стандартное поведение ссылки отключено */
+    event.preventDefault();
+
+    /* берем событие клика по объекту, берем у него target, достаем атрибут href(у того объекта по которму кликнули=target) и отсекаем у него первый символ */
+    const ID = event.target.getAttribute("href").substr(1);
+
+    /* нажав на ссылку, которая ведет к определенному id, мы плавно прокручиваемся к секции с этим id(getElementById(ID) получить элемент по id)*/
+    /* document.getElementById(ID).scrollIntoView({
+      behavior: "smooth",
+      /* в начало секции  
+      block: "start",
+    });*/
+
+    /* Кроссбраузерный вариант скролла */
+    seamless.scrollIntoView(document.getElementById(ID), {
+      behavior: "smooth",
+      block: "start",
+      inline: "center",
+    });
+  });
+});
+
 
